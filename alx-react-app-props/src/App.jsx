86befbe626b110e4dc007e0777827bd54b0f0acc
alikagwe <alikagwe@yahoo.com>
2025-09-07 23:29:47 +0300

    Props added

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
 function App() {
   const [count, setCount] = useState(0)
 
